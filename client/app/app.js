3a9d1748280032bafb0e18255c13a306417737f5
jdeveloperw <jdeveloperw@gmail.com>
2015-10-19 21:47:27 +0000

    Checkpoint before splitting into two

--- client/app/app.js
+++ client/app/app.js
@@ -3,8 +3,32 @@
 // Declare app level module which depends on views, and components
 angular.module('multisearch', [
 ])
-.controller('SearchController', ["$scope", function($scope) {
-  $scope.search = function() {
-    $scope.results = $scope.query;
+.factory('searchFactory', ['$http', function($http) {
+
+  var urlBase = 'http://localhost:8000/search/twitter?term=';
+  
+  var searchFactory = {};
+  
+  searchFactory.search = function(term) {
+      return $http.get(urlBase + term);
   };
-}]);
+  
+  return searchFactory;
+}])
+.controller('SearchController', ["$scope", "searchFactory", function($scope, searchFactory) {
+  $scope.searchInProgress = false;
+  
+  $scope.search = function() {
+    $scope.isSearchInProgress = true;
+    searchFactory.search($scope.query)
+      .success(function(value) {
+        $scope.results = value;
+        $scope.isSearchInProgress = false;
+      })
+      .error(function(error) {
+        $scope.results = error;
+        $scope.isSearchInProgress = false;
+      });
+  };
+}])
+;
